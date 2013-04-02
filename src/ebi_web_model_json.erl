0fe9d27ecf28f15de007f646b630128d35058cc9
Karolis Petrauskas <k.petrauskas@gmail.com>
2013-04-02 23:25:47 +0300

    JSON.

--- src/ebi_web_model_json.erl
+++ src/ebi_web_model_json.erl
@@ -1,8 +1,15 @@
 -module(ebi_web_model_json).
 -compile([{parse_transform, lager_transform}]).
--export([encode/1]).
+-export([encode/1, decode/2]).
+-export([encode_tstamp/1, decode_tstamp/1]).
 -include_lib("ebi_core/include/ebi.hrl").
 -include_lib("ebi_core/include/ebi_model.hrl").
+
+%%
+%% calendar:datetime_to_gregorian_seconds({{1970, 1, 1}, {0, 0, 0}}).
+%%
+-define(UNIX_BIRTH, 62167219200).
+-define(MEGA_SECS, 1000000).
 
 
 %% =============================================================================
@@ -48,9 +55,10 @@ encode(#ebi_species{name = Name, description = Description}) ->
         {description, encode_string(Description)}
     ]};
 
-encode(#ebi_reaction{name = Name, definition = Definition}) ->
+encode(#ebi_reaction{name = Name, description = Description, definition = Definition}) ->
     {[
         {name, encode_string(Name)},
+        {description, encode_string(Description)},
         {type, element(1, Definition)},
         {definition, encode(Definition)}
     ]};
@@ -69,8 +77,65 @@ encode(#ebi_rdef_simple{reagents = R, products = P, rateconst = C}) ->
         {reagents, [ F(X) || X <- R]},
         {products, [ F(X) || X <- P]},
         {rateconst, encode_string(C)}
+    ]};
+
+encode(#ebi_compartment{name = Name, description = Description, definition = Definition}) ->
+    {[
+        {name, encode_string(Name)},
+        {description, encode_string(Description)},
+        {type, element(1, Definition)},
+        {definition, encode(Definition)}
+    ]};
+
+encode(#ebi_cdef_solution{}) ->
+    {[
+    ]};
+
+encode(#ebi_cdef_diffusive{}) ->
+    {[
+    ]};
+
+encode(#ebi_cdef_insulating{}) ->
+    {[
+    ]};
+
+encode(#ebi_cdef_solid_electrode{}) ->
+    {[
     ]}.
 
+
+%%
+%%  Decode
+%%
+-spec decode(Type :: atom(), Json :: term()) -> term().
+decode(_, null) ->
+    undefined;
+
+decode(Type, List) when is_list(List) ->
+    [ decode(Type, Element) || Element <- List ];
+
+decode(model, {PL}) ->
+    #model{
+        id          = decode_string(proplists:get_value(id,          PL, null)),
+        ref         = decode_string(proplists:get_value(ref,         PL, null)),
+        name        = decode_string(proplists:get_value(name,        PL, null)),
+        description = decode_string(proplists:get_value(description, PL, null)),
+        status      = decode_atom  (proplists:get_value(status,      PL, null)),
+        changed     = decode_tstamp(proplists:get_value(changed,     PL, null)),
+        changed_by  = decode_string(proplists:get_value(changed_by,  PL, null)),
+        definition  = decode(ebi_model, proplists:get_value(definition,  PL, null)),
+        parameters  = case proplists:get_value(parameters,  PL, null) of
+            null -> undefined;
+            Prms -> lists:map(fun decode_string/1, Prms)
+        end
+    };
+
+decode(ebi_model, {PL}) ->
+    #ebi_model{
+        species      = decode(ebi_species,     proplists:get_value(species,      PL, null)),
+        reactions    = decode(ebi_reaction,    proplists:get_value(reactions,    PL, null)),
+        compartments = decode(ebi_compartment, proplists:get_value(compartments, PL, null))
+    }.
 
 
 %% =============================================================================
@@ -79,30 +144,75 @@ encode(#ebi_rdef_simple{reagents = R, products = P, rateconst = C}) ->
 
 
 %%
-%%
+%%  Encode everything to a json string.
 %%
 encode_string(undefined) ->
     null;
 
-encode_string(Text) ->
+encode_string(Text) when is_list(Text) ->
     list_to_binary(Text).
 
 
 %%
-%%
+%%  Encode timestamp.
 %%
 encode_tstamp(undefined) ->
     null;
 
-encode_tstamp({{Y, M, D}, {H, Mi, S}}) ->
-    Args = [Y, M, D, H, Mi, S],
-    Date = io_lib:format("~B-~2.10.0B-~2.10.0BT~2.10.0B:~2.10.0B:~2.10.0BZ", Args),
-    erlang:iolist_to_binary(Date);
-
-encode_tstamp(Now) ->
-    {_MegaSecs, _Secs, MicroSecs} = Now,
+encode_tstamp(Now = {_MegaSecs, _Secs, MicroSecs}) ->
     {{Y, M, D}, {H, Mi, S}} = calendar:now_to_datetime(Now),
     Args = [Y, M, D, H, Mi, S, MicroSecs],
     Date = io_lib:format("~B-~2.10.0B-~2.10.0BT~2.10.0B:~2.10.0B:~2.10.0B.~6.10.0BZ", Args),
+    erlang:iolist_to_binary(Date);
+
+encode_tstamp({{Y, M, D}, {H, Mi, S}}) ->
+    Args = [Y, M, D, H, Mi, S],
+    Date = io_lib:format("~B-~2.10.0B-~2.10.0BT~2.10.0B:~2.10.0B:~2.10.0BZ", Args),
     erlang:iolist_to_binary(Date).
+
+
+%%
+%%  Decode string.
+%%
+decode_string(null) ->
+    undefined;
+
+decode_string(Binary) when is_binary(Binary) ->
+    binary_to_list(Binary).
+
+
+%%
+%%  Decode atom.
+%%
+decode_atom(null) ->
+    undefined;
+
+decode_atom(Atom) when is_atom(Atom) ->
+    Atom;
+
+decode_atom(Binary) when is_binary(Binary) ->
+    erlang:binary_to_existing_atom(Binary).
+
+
+%%
+%%  Decode timestamp.
+%%
+decode_tstamp(null) ->
+    undefined;
+
+decode_tstamp(<<Year:4/binary, "-", Month:2/binary, "-", Day:2/binary, "T",
+                Hour:2/binary, ":", Min:2/binary,   ":", Sec:2/binary, Rest/binary>>) ->
+    Date = {
+        {binary_to_integer(Year), binary_to_integer(Month), binary_to_integer(Day)},
+        {binary_to_integer(Hour), binary_to_integer(Min), binary_to_integer(Sec)}
+    },
+    decode_tstamp(Date, Rest).
+
+decode_tstamp(Date, <<".", MSec:6/binary, "Z">>) ->
+    DateSecs = calendar:datetime_to_gregorian_seconds(Date) - ?UNIX_BIRTH,
+    {DateSecs div ?MEGA_SECS, DateSecs rem ?MEGA_SECS, binary_to_integer(MSec)};
+
+decode_tstamp(Date, <<"Z">>) ->
+    Date.
+
 
---
+++ test/ebi_web_model_json_tests.erl
@@ -0,0 +1,106 @@
+%
+% Copyright 2012 Karolis Petrauskas
+%
+% Licensed under the Apache License, Version 2.0 (the "License");
+% you may not use this file except in compliance with the License.
+% You may obtain a copy of the License at
+%
+%     http://www.apache.org/licenses/LICENSE-2.0
+%
+% Unless required by applicable law or agreed to in writing, software
+% distributed under the License is distributed on an "AS IS" BASIS,
+% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+% See the License for the specific language governing permissions and
+% limitations under the License.
+%
+-module(ebi_web_model_json_tests).
+-include_lib("eunit/include/eunit.hrl").
+-include_lib("ebi_core/include/ebi.hrl").
+-include_lib("ebi_core/include/ebi_model.hrl").
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%  Tests
+%%
+
+timestamp_secs_test() ->
+    Str = <<"2013-04-02T23:06:17Z">>,
+    Date = {{2013, 04, 02}, {23, 06, 17}},
+    Str = ebi_web_model_json:encode_tstamp(Date),
+    Date = ebi_web_model_json:decode_tstamp(Str),
+    ok.
+
+timestamp_msec_test() ->
+    Str = <<"2013-04-02T19:39:08.646842Z">>,
+    Date = {1364,931548,646842},
+    Str = ebi_web_model_json:encode_tstamp(Date),
+    Date = ebi_web_model_json:decode_tstamp(Str),
+    ok.
+
+
+encode_decode_test() ->
+    _EbiModel = #ebi_model{
+        species = [
+            #ebi_species{name = "S", description = "Substrate"},
+            #ebi_species{name = "P", description = "Product"},
+            #ebi_species{name = "Eo", description = "Enzyme in the oxidized form"},
+            #ebi_species{name = "Er", description = "Enzyme in the reduced form"}
+        ],
+        reactions = [
+            #ebi_reaction{name = "R1", description = "First reaction", definition = #ebi_rdef_mm{
+                substrate = "S",
+                product = "P",
+                vmax = "Vmax",
+                km = "KM"
+            }},
+            #ebi_reaction{name = "R2", description = "Second reaction", definition = #ebi_rdef_simple{
+                reagents = [{"Er", 1}, {"S", 1}],
+                products = [{"Eo", 1}, {"P", 1}],
+                rateconst = "k1"
+            }}
+        ],
+        compartments = [
+            #ebi_compartment{
+                name = "\\Omega_1",
+                description = "Solid electrode",
+                definition = #ebi_cdef_solid_electrode{
+                    el_reaction = "R1"
+                }
+            },
+            #ebi_compartment{
+                name = "\\Omega_2",
+                description = "Enzyme",
+                definition = #ebi_cdef_diffusive{
+                    reactions = ["R2"],
+                    %diffusion_coef = "De",
+                    diffusion_coefs = []
+                }
+            },
+            #ebi_compartment{
+                name = "\\Omega_3",
+                description = "Solution",
+                definition = #ebi_cdef_solution{
+                    nernst_thickness = "d2",
+                    concentrations = [{"S", "S0"}]
+                }
+            }
+        ]
+    },
+    Model = #model{
+        id = "M-10012",
+        ref = "0123456789012345678901234567890123456789",
+        name = "Name",
+        description = "Desc",
+        status = active,
+        changed = erlang:now(),
+        changed_by = "Jonas Jonaitis",
+        definition = undefined,
+        parameters = ["asd"],
+        representations = []
+    },
+    Model = ebi_web_model_json:decode(model, ebi_web_model_json:encode(Model)),
+    ok.
+
+
+
+
